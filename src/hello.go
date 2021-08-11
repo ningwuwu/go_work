ee92a5ff926f281cae293718e08250288fe1ba3e
ningwuwu <13227791021@163.com>
2021-08-11 20:46:12 +0800

    2021-08-11 匿名函数使用

--- src/hello.go
+++ src/hello.go
@@ -87,9 +87,72 @@ func test2() {
 		i++
 	}
 }
+type casefunc func(val int) bool
 func main() {
 	//test1()
-	test2()
+	//test2()
 	//a := '一'
 	//fmt.Printf("%T, %v", a, a)
+	//arr := []int {0, 1, 2, 3, 4, 5, 6}
+	//res := filter(arr, isEven)
+	//fmt.Println(res)
+	test3()
+	// 比如说这里，res1得到的是匿名函数的地址，
+	// 和counter这个函数没有任何关系，
+	// 他只是一个代理作用，帮你得到这个匿名函数
+	// 所以就算再counter中修改变量的值也不会影响匿名函数中值的改变
+	res1 := counter()
+	fmt.Println(res1())
+	fmt.Println(res1())
+	// 这里如果重新引用了这个匿名函数，
+	// 那么他和之前的匿名函数使用完全没有关联
+	// 所以res2虽然和res1保存都是这个匿名函数的地址
+	// 但是对于匿名函数的使用是完全分开的
+	// res1还是继承上一次res1++,res2从0重新开始
+	// 注意：使用匿名函数的使用一定要加括号，
+	// 只有添加括号才认为你是通过这个函数地址去执行这个函数
+	// 变量中存的是返回值，否则存的就是函数的地址
+	res2 := counter()
+	fmt.Println(res2())
+	fmt.Println(res1())
+	fmt.Printf("%T\n", res1)
+	fmt.Printf("%T", res1())
+}
+func isEven(val int) bool {
+	if val % 2 == 0 {
+		return true
+	}
+	return false
+}
+/*func isOdd(val int) bool {
+	if val % 2 == 0
+		return false
+	return true
+}*/
+// 函数做变量，回调函数
+func filter(arr []int, myfunc casefunc) []int {
+	var res []int
+	for _, val := range arr {
+		if myfunc(val) {
+			res = append(res, val)
+		}
+	}
+	return res
+}
+// 闭包函数
+// 匿名函数的使用实际上就是直接访问这个匿名函数指针
+// 所以他带有记忆属性
+func test3() {
+	res := func() int{
+		i := 3
+		return i
+	}()
+	fmt.Println(res)
+}
+func counter() (func() int) {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
